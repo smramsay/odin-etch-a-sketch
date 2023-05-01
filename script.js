503700106ba2af3d0f358472dfdf82a3b28624f1
Scott Ramsay <7002566+smramsay@users.noreply.github.com>
2023-05-01 16:50:54 -0700

    Add grid creation and button action for new grid

---
+++ script.js
@@ -0,0 +1,27 @@
+const gridContainer = document.querySelector('.grid-container');
+
+function createGrid(gridSize) {
+    for (let i = 0; i < gridSize; i++) {
+        const gridLine = document.createElement('div');
+        gridLine.classList.add('grid-line');
+        for (let k = 0; k < gridSize; k++) {
+            const gridSquare = document.createElement('div');
+            gridSquare.classList.add('square');
+            gridLine.appendChild(gridSquare);
+        }
+        gridContainer.appendChild(gridLine);
+    }
+}
+
+function promptNewGrid() {
+    newSize = parseInt(prompt("Enter size of new grid"));
+    Array.from(gridContainer.children).forEach(child => child.remove());
+    createGrid(newSize);
+}
+
+const INIT_GRID_SIZE = 16;
+
+createGrid(INIT_GRID_SIZE);
+
+const newGridButton = document.querySelector('button');
+newGridButton.addEventListener('click', promptNewGrid);
